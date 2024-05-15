6586eb130d953dac57b0e4c4994787e96e566230
ErodeesFleurs <3353309908@qq.com>
2024-05-15 22:45:37 +0800

    Oops

--- src/Promise.hpp
+++ src/Promise.hpp
@@ -3,6 +3,7 @@
 #include <future>
 #include <thread>
 #include <chrono>
+#include <string>
 
 #include "sol/sol.hpp"
 
@@ -20,9 +21,53 @@ namespace Promise {
 
         sol::object result(sol::this_state L) const;
         
-        std::string error();
+        std::string error() const;
 
     private:
         std::shared_ptr<std::future<ResultType>> m_promise;
     };
+
+    template <typename ResultType>
+    bool Promise<ResultType>::finished() const {
+        return m_promise->wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+    }
+
+    template <typename ResultType>
+    bool Promise<ResultType>::succeeded() const {
+        if (finished()) {
+            try {
+                m_promise->get();
+                return true;
+            } catch (...) {
+                return false;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    template <typename ResultType>
+    sol::object Promise<ResultType>::result(sol::this_state L) const {
+        if (finished()) {
+            try {
+                return sol::make_object(L, m_promise->get());
+            } catch (...) {
+                return sol::make_object(L, sol::nil);
+            }
+        }
+        return sol::make_object(L, sol::nil);
+    }
+
+    template <typename ResultType>
+    std::string Promise<ResultType>::error() const {
+        if (finished()) {
+            try {
+                m_promise->get();
+                return "";
+            } catch (std::exception& e) {
+                return e.what();
+            }
+        }
+        return "";
+    }
 }
