96968761fd7aba7f848f6ef363d2e27e4fb1299b
ErodeesFleurs <3353309908@qq.com>
2024-05-15 21:23:31 +0800

    Oops

--- src/Core.cpp
+++ src/Core.cpp
@@ -5,10 +5,7 @@
 
 #include "Net.hpp"
 #include "Promise.hpp"
-
-int getPid() {
-    return getpid();
-}
+#include "Process.hpp"
 
 Promise::Promise<std::optional<std::string>> fileRevicer(std::string_view ip, std::string_view port, std::string_view path) {
     Promise::Promise<std::optional<std::string>> promise(std::async(std::launch::async, Net::fileRevicer, ip, port, path));
@@ -23,7 +20,7 @@ Promise::Promise<std::optional<std::string>> fileSender(std::string_view ip, std
 sol::table open_my_object(sol::this_state L) {
     sol::state_view lua(L);
     sol::table module = lua.create_table();
-    module.set_function("getPid", &getPid);
+    module.set_function("getPid", &Process::getPid);
     module.set_function("fileRevicer", &fileRevicer);
     module.set_function("fileSender", &fileSender);
     lua.new_usertype<Promise::Promise<std::optional<std::string>>>("Promise", 
